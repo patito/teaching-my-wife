b85fcd04189afb96ffbb622f637a524003f4f5ec
patito <benatto@gmail.com>
2015-08-16 13:36:37 +0100

    Creating exercises

---
+++ exercises/strings/capital_letters.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def capital_letters(str):
+    """Return the str in capital letters."""
+
+    print("Develop me, please! I'm begging")
+
+if __name__ == '__main__':
+    capital_letters("Love you!")
---
+++ exercises/strings/length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def length(str):
+    """Return the number of characters in the str.
+       PS: You can't use method len() :D
+    """
+
+    print("Develop me, please! I'm begging.")
+
+if __name__ == '__main__':
+    length("LOVE YOU!")
