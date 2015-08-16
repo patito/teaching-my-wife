7f023450dc8f2ef8100115fe4e9e142342cf12bb
patito <benatto@gmail.com>
2015-08-16 14:13:59 +0100

    miscelaneous exercises

---
+++ exercises/misc/celsius_to_fahrenheit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def celsius_to_fahrenheit(c_temp):
+    """Convert celsius temperature to fahrenheit."""
+
+    print("Develop me, please! I'm begging.")
+
+if __name__ == '__main__':
+    celsius_to_fahrenheit(30)
---
+++ exercises/misc/fahrenheit_to_celsius.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+def fahrenheit_to_celsius(temperature):
+    """Convert fahrenheit temperature to celsius."""
+
+    print("Develop me, please! I'm begging.")
+
+if __name__ == '__main__':
+    fahrenheit_to_celsius(60)
